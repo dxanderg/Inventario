e54a447cd5b9f82019535a6249152a17c2859c90
Diego Rodriguez <dxanderg@gmail.com>
2017-07-05 06:01:24 -0500

    Traslados Form - Avance

--- controllers/trasladoscontroller.js
+++ controllers/trasladoscontroller.js
@@ -46,19 +46,6 @@ module.exports = {
 					callback()
 	      })
   		}
-  		// function(callback) { db.query(`select a.id_articulos, a.activo, e.nombre_estado, a.serial_art, a.plaqueta_art, a.fk_items, i.nombre_item, i.modelo_item, a.fk_puesto, a.fk_campaign, c.nombre_campaign, f.nombre_fabricante, a.fk_bodega, b.nombre_bodega, a.fk_sede, s.nombre_sede from articulos a
-				// 															JOIN estados e ON a.activo = e.id_estados
-				// 															JOIN bodegas b ON a.fk_bodega = b.id_bodega
-				// 															JOIN sedes s ON a.fk_sede = s.id_sede 
-				// 															JOIN puestos p ON a.fk_puesto = p.id_puesto
-				// 															JOIN campaign c ON a.fk_campaign = c.id_campaign
-				// 															JOIN items i ON a.fk_items = i.id_item
-				// 															JOIN fabricante f ON i.fk_fabricante = f.id_fabricante
-				// 															where a.id_articulos = ?`, id, function(err, rows, fields){
-				// 	if(err) throw err
-				// 	consulta6 = rows
-				// 	callback()
-	   //    })
 		], function(err, results) {
   		res.render('Traslados', {consulta1 : consulta1, consulta2 : consulta2, consulta3 : consulta3, consulta4: consulta4, consulta5 : consulta5})
 		})
@@ -79,50 +66,49 @@ module.exports = {
 		    {
 		      'dest_sede' : {
 		      		string: '(SELECT fk_sede from ?? WHERE id_puesto = ?)',
-		      		value: [puestos, req.body.puestoF1]
+		      		value: [puestos, req.body.posicionF1]
 		      },
 		      'dest_bodega': {
 		      		string: '(SELECT fk_bodega from ?? WHERE id_puesto = ?)',
-		      		value: [puestos, req.body.puestoF1]
+		      		value: [puestos, req.body.posicionF1]
 		      },
 		      'dest_posicion': {
 		      		string: '(SELECT id_puesto from ?? WHERE id_puesto = ?)',
-		      		value: [puestos, req.body.puestoF1]
+		      		value: [puestos, req.body.posicionF1]
 		      },
 		      'dest_campaña': {
 		      		string: '(SELECT fk_campaign from ?? WHERE id_puesto = ?)',
-		      		value: [puestos, req.body.puestoF1]
+		      		value: [puestos, req.body.posicionF1]
 		      },
 		      'ori_sede': {
 		      		string: '(SELECT fk_sede from ?? WHERE fk_puesto = ? AND fk_tipo = ?)',
-		      		value: [articulos, req.body.puestoI1, req.body.item1]
+		      		value: [articulos, req.body.posicionI1, req.body.item1]
 		      },
 		      'ori_bodega': {
 		      		string: '(SELECT fk_bodega from ?? WHERE fk_puesto = ? AND fk_tipo = ?)',
-		      		value: [articulos, req.body.puestoI1, req.body.item1]
+		      		value: [articulos, req.body.posicionI1, req.body.item1]
 		      },
 		      'ori_posicion': {
 		      		string: '(SELECT fk_puesto from ?? WHERE fk_puesto = ? AND fk_tipo = ?)',
-		      		value: [articulos, req.body.puestoI1, req.body.item1]
+		      		value: [articulos, req.body.posicionI1, req.body.item1]
 		      },
 		      'ori_campaña': {
 		      		string: '(SELECT fk_campaign from ?? WHERE fk_puesto = ? AND fk_tipo = ?)',
-		      		value: [articulos, req.body.puestoI1, req.body.item1]
+		      		value: [articulos, req.body.posicionI1, req.body.item1]
 		      },
 		      'ticket': req.body.ticket ,
 		      'fecha_mov': fechaA ,
 		      'fk_usuario': usuario ,
 		      'fk_articulos': {
 		      	string: '(SELECT id_articulos from ?? WHERE fk_puesto = ? AND fk_tipo = ?)',
-		      		value: [articulos, req.body.puestoI1, req.body.item1]
+		      		value: [articulos, req.body.posicionI1, req.body.item1]
 		      }
 		    },
 		  ]
 		})
-		console.log(req.body['opcion1'])
-		console.log(req.body.opcion1)
-		console.log(req.body.puestoI1)
-		console.log(req.body.puestoF1)
+		console.log(req.body.item1)
+		console.log(req.body.posicionI1)
+		console.log(req.body.posicionF1)
 
 		var config = require('.././database/config')
 		var db = mysql.createConnection(config)
