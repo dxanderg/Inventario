5e11ffb92ffee2e098606b823a9b4bfc0cf610da
Diego Rodriguez <dxanderg@gmail.com>
2017-07-20 14:20:07 -0500

    Chart JS Pruebas

--- controllers/homecontroller.js
+++ controllers/homecontroller.js
@@ -1,4 +1,5 @@
 var mysql = require('mysql')
+var _ = require('lodash')
 
 module.exports = {
 
@@ -9,11 +10,37 @@ module.exports = {
 
 		var sedes = null
 
-		db.query('SELECT * from sedes', function(err, rows, fields){
+		db.query('select b.nombre_bodega, count(puestos.id_puesto) as puestos from puestos join bodegas b ON puestos.fk_bodega = b.id_bodega group by fk_bodega', function(err, rows, fields){
 			if(err) throw err
 			sedes = rows
+			var data = []
+			var player = [];
+			var score = [];
+
+			for(var i in sedes) {
+				player.push("Player " + sedes[i].nombre_bodega);
+				score.push(sedes[i].puestos);
+			}
+			var chartdata = {
+				labels: player,
+				datasets : [
+					{
+						label: 'Player Score',
+						backgroundColor: 'rgba(200, 200, 200, 0.75)',
+						borderColor: 'rgba(200, 200, 200, 0.75)',
+						hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
+						hoverBorderColor: 'rgba(200, 200, 200, 1)',
+						data: score
+					}
+				]
+			};
+
+     // var data = _.map(rows, function(n) { //here using lodash
+     //     return [`[{nombre:"` + n.nombre_bodega + `", cantidad:"` + n.puestos + `"}]`];
+     // })
 			db.end()
-		res.render('index', {sedes : sedes})
+		res.render('index', {sedes : data, datos : JSON.stringify(sedes), joder : JSON.stringify(chartdata)})
+		
 		})
 	},
 
--- public/javascripts/chartinit.js
+++ public/javascripts/chartinit.js
@@ -1,45 +1,36 @@
 $(document).ready(function(){
-	var $row = $(this).closest("tr");    // Find the row
-	var $tds = $row.find("td");
-	$.each($tds, function() {
-	        console.log($(this).text());
+
+	var element = document.querySelector('meta[name="lolo"]');
+	console.log(element)
+	var content = element && element.getAttribute("content");
+	console.log(content)
+	// var content2 = JSON.parse(content)
+	// console.log(content2)
+
+	// var array = $.map(content, function(value, index) {
+	//     return [value];
+	// });
+	// console.log(array);
+
+	// https://www.dyclassroom.com/chartjs/chartjs-how-to-draw-bar-graph-using-data-from-mysql-table-and-php
+
+	var datas = {
+    labels: [element],
+    datasets: [
+        {
+          label: "My First dataset",
+          data: [content],
+          backgroundColor: ['#F44336','#2196F3','#CDDC39','#673AB7','#4CAF50','#FF9800','#673AB7','#FFEB3B','#795548','#607D8B','#00BCD4'],
+		      borderColor: ['white'],
+		      borderWidth: 2
+		    }
+		]
+	}
+
+	var ctx = document.getElementById("myDonutChart").getContext('2d');
+	var myDoughnutChart = new Chart(ctx, {
+	    type: 'doughnut',
+	    data: content
 	});
+
 })
-
-var sedes = document.getElementById("datoschart")
-
-
-var ctx = document.getElementById("myDonutChart").getContext('2d');
-var myDoughnutChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: sedes
-});
-
-
-
- var datas = {
-            labels: ["January", "February", "March", "April", "May", "June", "July"],
-            datasets: [
-                {
-                    label: "My First dataset",
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                '#F44336',
-                '#2196F3',
-                '#CDDC39',
-                '#673AB7',
-                '#4CAF50',
-                '#FF9800',
-                '#673AB7',
-                '#FFEB3B',
-                '#795548',
-                '#607D8B',
-                '#00BCD4'
-            ],
-            borderColor: [
-                'white'
-            ],
-            borderWidth: 2
-                }
-            ]
-        }
